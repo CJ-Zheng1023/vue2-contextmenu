159c3e40924afcc2451052bb4994274b5700f7ca
CJ-Zheng1023 <CJ_Zheng1023@hotmail.com>
2021-01-20 13:23:02 +0800

    base

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,3 @@
-const path = require('path')
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
-}
 module.exports = {
   pages: {
     index: {
@@ -9,10 +5,5 @@ module.exports = {
       template: 'public/index.html',
       filename: 'index.html'
     }
-  },
-  chainWebpack: config => {
-    config.module.rule('js').include.add(resolve('src')).end().use('babel').loader('babel-loader').tap(options => {
-      return options
-    })
   }
 }
